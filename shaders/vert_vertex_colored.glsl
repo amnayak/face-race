b37f3459318c5a701b1bc7f8ac20afae7ea8c86d
Adrian Biagioli <adrianbiagioli@gmail.com>
2018-10-13 19:54:44 -0400

    Add shader loader from file

---
+++ shaders/vert_vertex_colored.glsl
@@ -0,0 +1,19 @@
+#version 330
+uniform mat4 object_to_clip;
+uniform mat4x3 object_to_light;
+uniform mat3 normal_to_light;
+
+// note: layout keyword used to make sure that the location-0 ttribute is always bound to something
+layout(location=0) in vec4 Position;
+in vec3 Normal;
+in vec4 Color;
+out vec3 position;
+out vec3 normal;
+out vec4 color;
+
+void main() {
+       gl_Position = object_to_clip * Position;
+       position = object_to_light * Position;
+       normal = normal_to_light * Normal;
+       color = Color;
+}
